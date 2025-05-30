c9f4e633e45783e0e0bc74749687a7644c631cec
BikashChamua01 <bikashchamua537@gmail.com>
2025-05-30 13:32:36 +0530

    use of useCallBack

--- src/001component/index.jsx
+++ src/001component/index.jsx
@@ -1,59 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import Card from "./Card";
 import Filter from "./Filter";
 import Sort from "./Sort";
+
 const URL =
   "https://api.themoviedb.org/3/movie/popular?api_key=38d814eb199b0d52d6c828897b9107af";
 
-// rating filter //
-export const rangeFilter = (event, filterProducts, setFilterProducts) => {
-  let val = Number(event.target.value);
-  console.log(filterProducts);
-  let newFilterProducts;
-  newFilterProducts = filterProducts.filter((product) => {
-    return Number(product.vote_average) > val;
-  });
-
-  setFilterProducts(newFilterProducts);
-};
-
 const Netflix = () => {
   const [movies, setMovies] = useState([]);
-  const [filterProducts, setFilterProducts] = useState(movies);
+  const [filterProducts, setFilterProducts] = useState([]);
+  const [sortOrder, setSortOrder] = useState(null); // 'a-z', 'z-a', 'rating'
+  const [ratingThreshold, setRatingThreshold] = useState(0);
 
   async function getData(URL) {
     try {
       const res = await axios.get(URL);
       setMovies(res.data.results);
-      setFilterProducts(res.data.results);
     } catch (err) {
       console.log(err);
     }
   }
+
   useEffect(() => {
     getData(URL);
   }, []);
+
+  const handleFilterAndSort = useCallback(() => {
+    let updated = [...movies];
+
+    // Apply rating filter
+    if (ratingThreshold) {
+      updated = updated.filter(
+        (movie) => movie.vote_average >= ratingThreshold
+      );
+    }
+
+    // Apply sort
+    if (sortOrder === "a-z") {
+      updated.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sortOrder === "z-a") {
+      updated.sort((a, b) => b.title.localeCompare(a.title));
+    } else if (sortOrder === "rating") {
+      updated.sort((a, b) => b.vote_average - a.vote_average);
+    }
+
+    setFilterProducts(updated);
+  }, [movies, sortOrder, ratingThreshold]);
+
+  useEffect(() => {
+    handleFilterAndSort();
+  }, [handleFilterAndSort]);
+
+  const handleSortChange = (order) => {
+    setSortOrder(order);
+  };
+
+  const handleRatingChange = (threshold) => {
+    setRatingThreshold(threshold);
+  };
 
   return (
     <>
       <header className="flex-1 text-5xl text-shadow-2xs text-center pb-6">
         Movies
       </header>
+
       <div className="w-screen flex align-middle justify-between p-10">
         <div className="filter-section">
-          <Filter
-            filterProducts={movies}
-            setFilterProducts={setFilterProducts}
-          />
+          <Filter onRatingChange={handleRatingChange} />
         </div>
         <div className="sort-section">
-          <Sort
-            filteredMovies={filterProducts}
-            setFilteredMovies={setFilterProducts}
-          />
+          <Sort onSortChange={handleSortChange} />
         </div>
       </div>
+
       <div className="movies flex flex-wrap justify-around">
         {filterProducts.map((movie) => {
           return <Card key={movie.id} movie={movie} url={URL} />;
